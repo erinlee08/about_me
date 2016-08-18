eda9bb63fec552ee63c9513553295af5f7212cf7
Erin <erinylee@yahoo.com>
2016-08-18 00:24:37 -0700

    added more y/n questions

--- app.js
+++ app.js
@@ -4,17 +4,31 @@ console.log(user);
 
 alert('Welcome ' + user + '!');
 
-var color = prompt('Fun time!Guessing game! what my fav color is! Red, Blue, or White?');
-console.log(color);
+var question = prompt('Want to play some games now?');
+console.log(question);
 
-if (color.toLowerCase() === 'red') {
-  console.log('YES! ', color , ' is a POP of color. My fav too!' , user);
-} else if (color.toLowerCase() === 'blue' ){
-  console.log(color , ' is like the sky!', user);
-} else if (color.toLowerCase() === 'white') {
-  console.log(color , ' is clean', user);
+if (question.toLowerCase() === 'y') {
+  console.log('Great!');
 } else {
-  console.log(user , ' Different shades of colors!');
+  console.log('Oh man! Too bad, we are playing games anyway!');
+};
+
+var color = ['Red' , 'Blue' , 'White' , 'Black', 'Brown'];
+var answer = prompt('Fun time!!What is my favorite color?');
+console.log(color);
+var flag;
+var count = 0;
+
+for (var i = 0; i < color.length; i++) {
+  console.log(answer[i]);
+
+  if (color[i] === 'Red') {
+    alert('YES! ', color , ' is a POP of color. My fav too!');
+    flag;
+  } else {
+    alert('Keep trying');
+    count++;
+  };
 };
 
 var code = prompt('Next, is coding fun?');
@@ -24,4 +38,33 @@ if (code === 'Y') {
   console.log(user, 'So far it is!!');
 } else {
   console.log('Keep going!');
+};
+
+var workWork = prompt('Do you think I find it lots of working, learning how to code?tur or false');
+console.log(workWork);
+if (true) {
+  console.log('Yes, it is!');
+} else {
+  console.log('Show me what your trick is making it easy to learn coding!!');
+};
+
+}
+var work = prompt('Besides coding, I was in dental field for a while. Guess how long I have been doing that?');
+console.log(work);
+
+if (work >= 2) {
+  alert('Guessed too low');
+  count++;
+} else if (work >= 4) {
+  alert('Getting warmer!');
+  count++;
+} else if (work > 8) {
+  alert('Whoa,, guessed too high!');
+  count++;
+}else if (work === 8 ) {
+  alert('Bingo!');
+  count++;
+} else {
+  alert('Keep trying!');
+  count++;
 };
