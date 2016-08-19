5db15ac9d4306bc05d5c10ea4475e387f54a529a
Erin <erinylee@yahoo.com>
2016-08-18 17:04:50 -0700

    merged code

--- app.js
+++ app.js
@@ -17,50 +17,54 @@ if ( question.toLowerCase() === 'y' || question.toLowerCase() === 'Y' ) {
 };
 // question 2
 var color = ['Red' , 'Blue' , 'White' , 'Black', 'Brown'];
-var answer = prompt('Fun time!!What is my favorite color?');
+var answer;
 console.log(color);
-var count = 0;
 var guess = 0;
+var flag = false;
 
-while ( guess < 6) {
-  answer = prompt('Fun time!!What is my favorite color?');
+while ( guess < 5) {
+  var answer = prompt('Fun time!!What is my favorite color?');
   for (var i = 0; i < color.length; i++) {
-    console.log(answer[i]);
-    if (color[i] === 'Red') {
-      alert('YES! ', color , ' is a POP of color. My fav too!');
-      break;
-    } else {
-      alert('Keep trying');
-      count++;
-    };
+      console.log(color[i]);
+    if (answer === 'Red' || answer === 'red') {
+      alert('YES! ' + answer + ' is a POP of color. My fav too!');
+      flag === true;
+      // break;
+    }
   };
+
+  if (!flag) {
+    alert('keep trying');
+    guess++;
+    console.log(guess);
+  } else {
+    break;
+  }
 };
-// question 4
+// question 3
 var code = prompt('Next, is coding fun? Please type in Y or N');
 console.log(code);
-
 if (code === 'Y') {
-  console.log('So far it is!!');
+  alert('So far it is!!');
 } else {
   console.log('Keep going!');
 };
-
 // question 4
-var workWork = prompt('Is learning how to code hard?true or false');
-console.log(workWork);
+var work = prompt('Is learning how to code hard?true or false');
+console.log(work);
 if ( true || 't') {
   alert('Yes, it is!');
 } else {
   alert('Show me what your trick is making it easy to learn coding!!');
 };
-
-var good = prompt('I know coding is hard, but I will be successful. Right?');
-console.log(good);
-if( good === 'Y' || good === 't') {
-  console.log(good);
+//question 5
+var fun = prompt('I know coding is hard, but it can also be fun, Right?');
+console.log(fun);
+if( fun === 'Y' || fun === 't') {
+  console.log(fun);
   alert('I think so too!');
 } else {
-  console.log(good);
+  console.log(fun);
   alert('boo!');
 };
 
